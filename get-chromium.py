393ddb95be324673c3ff963d1fca0b93e217fe57
iceTwy <nerorush23@gmail.com>
2013-08-25 23:25:11 +0200

    Major code rewrite, add support for download directory selection
    Functions now return variables, which are then called in main and used as arguments for other functions. Globals have been removed. The user can now select their default download directory by typing the path when prompted.
    
    The user will be prompted to create the directory ~/.git-chromium.
    The latest snapshot downloaded has its revision number saved into ~/.git-chromium/LAST_INSTALL and the path to the download directory is saved into ~/.git-chromium/DOWNLOAD_DIR.

--- get-chromium.py
+++ get-chromium.py
@@ -1,23 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#		get-chromium - A Python script to retrieve the latest Chromium snapshots.
+#		git repository: https://github.com/iceTwy/get-chromium.git
+#		website: https://github.com/iceTwy/get-chromium
+#	
+#		author: iceTwy <icetwy@inventati.org> (icetwy.re)
+#		
+# LICENSE
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+# 
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# 
+# For more information, please refer to <http://unlicense.org/>
+#
 
 import urllib
 import urllib2
 import sys
 import platform
-
-global userOS; userOS = platform.system()
-global userArch; userArch = platform.machine()
-
-global revisionFile; revisionFile = 'LAST_INSTALL'
-
+import os
 	
-def createStringFromOS():
-	
+def BuildURLFromOS():
 	osString = ['Mac', 'Win', 'Linux', 'Linux_x64', 'Linux_ARM_Cross-Compile', 'Android']
 
-	global osStringAppend
-	global dlfile
+	userOS = platform.system()
+	userArch = platform.machine()
 	
 	if userOS == 'Darwin':
 		osStringAppend = osString[0]
@@ -54,65 +82,114 @@ def createStringFromOS():
 	else:
 		print 'Platform not supported'
 		sys.exit()
+		
+	return osStringAppend, dlfile
 	
-def findSnapshotVer():
-	global SnapshotVer
-	
+def FindSnapshotRevision(osStringAppend):
 	try:
-		getSnapshotVer = urllib2.urlopen('https://commondatastorage.googleapis.com/chromium-browser-snapshots/' + osStringAppend + '/LAST_CHANGE', timeout=30)
+		getSnapshotRev = urllib2.urlopen('https://commondatastorage.googleapis.com/chromium-browser-snapshots/' + osStringAppend + '/LAST_CHANGE', timeout=30)
 	except urllib2.URLError:
 		print "Couldn't access to the requested URL.\nCheck your Internet connection settings.\nExiting..."
 		sys.exit()
 		
-	SnapshotVer = getSnapshotVer.read()
+	SnapshotRev = getSnapshotRev.read()
+	print "Available Chromium snapshot revision: %s" % SnapshotRev
 	
-	print "Available Chromium snapshot revision: %s" % SnapshotVer
+	return SnapshotRev
 	
-def checkPriorInstall():
+def CheckDirsFiles():
+	SettingsDir = os.path.expanduser("~/.get-chromium") # default directory to save get-chromium settings
+	DLDirFile = os.path.expanduser("~/.get-chromium/DOWNLOAD_DIR") # file containing userDLDir
+	RevisionFile = os.path.expanduser("~/.get-chromium/LAST_INSTALL") # file containing SnapshotRev
+		
+#SettingsDir check
+	check_SettingsDir = os.path.exists(SettingsDir)
 	
-	try:
-		with open(revisionFile): pass
-	except IOError:
+	if check_SettingsDir == False:
+		os.mkdir(SettingsDir)
+	else:
+		pass
+		
+#RevisionFile check
+	check_RevisionFile = os.path.exists(RevisionFile)
+	
+	if check_RevisionFile == False:
 		print "\nIt seems that this is the first time you fetch a Chromium snapshot.\nWould you like to create a file that keeps in memory the latest revision downloaded?\nThis will avoid redownloading Chromium if you already have the latest revision."
 		createNewFile = raw_input("Create a file to remember the last downloaded revision? (y/n) ")
 		if createNewFile != 'y' and createNewFile != 'n':
 			sys.exit()
-		else:
-			touchRevisionFile = open(revisionFile, 'w+') # simply creating the file, it's updated with logNewInstall()
+		elif createNewFile == 'y':
+			touchRevisionFile = open(RevisionFile, 'w+') # simply creating the file, it's updated with logNewInstall()
 			touchRevisionFile.close()
 			print "Revision file successfully created!"
-			
-	try:
-		with open(dlfile): pass
-		
-		openRevisionFile = open(revisionFile, 'r')
-		checkRevision = openRevisionFile.read()
-		
-		if checkRevision:
-			print "Downloaded Chromium snapshot revision: %s" % checkRevision
-			
-			if SnapshotVer == checkRevision:
-				print "You have already downloaded the latest Chromium snapshot.\nExiting..."
-				sys.exit()
-				
-			elif SnapshotVer > checkRevision:
-				print "The last revision you have downloaded is outdated.\nUpdating..."
-		
 		else:
-			pass
-			
-		openRevisionFile.close()
+			print "Revision file not created. Revision number will not be tracked."
+	else:
+		pass
 		
-	except IOError:
-		print "You have not yet downloaded any Chromium snapshot, or they are stored in another directory. Downloading..."
+#DLDirFile check
+	check_DLDirFile = os.path.exists(DLDirFile)
 	
-def getSnapshot():
-	
-	SnapshotURL = 'https://commondatastorage.googleapis.com/chromium-browser-snapshots/' + osStringAppend + '/' + SnapshotVer + '/' + dlfile
+	if check_DLDirFile == False:
+		create_DLDirFile = raw_input("You haven't defined a default download directory. Would you like to select one? (y/n) ")
+		
+		if create_DLDirFile != 'y' and createDLDirFile != 'n':
+			sys.exit()
+		
+		elif create_DLDirFile == 'y':
+				touch_DLDirFile = open(DLDirFile, 'w')
+				choose_DLDir = raw_input("Choose a default download directory (e.g. ~/get-chromium): ")
+				touch_DLDirFile.write(choose_DLDir)
+				touch_DLDirFile.close()
+				print "Default download directory successfully chosen!"
+				
+				open_DLDirFile = open(DLDirFile, 'r')
+				userDLDir = open_DLDirFile.read()
+				open_DLDirFile.close()
+		else:
+			print "Snapshots will be downloaded to the current directory."
+	else:
+		open_DLDirFile = open(DLDirFile, 'r')
+		userDLDir = open_DLDirFile.read()
+		if not userDLDir:
+			touch_DLDirFile = open(DLDirFile, 'w')
+			choose_DLDir = raw_input("Choose a default download directory (e.g. ~/get-chromium): ")
+			touch_DLDirFile.write(choose_DLDir)
+			touch_DLDirFile.close()
 			
+			userDLDir = open_DLDirFile.read()
+			open_DLDirFile.close()
+		else:
+			open_DLDirFile.close()
+			
+	return RevisionFile, userDLDir
+
+def CheckPriorRevision(RevisionFile, SnapshotRev):
+	openRevisionFile = open(RevisionFile, 'r')
+	checkRevision = openRevisionFile.read()
+		
+	if checkRevision:
+		print "Downloaded Chromium snapshot revision: %s" % checkRevision
+		
+		if SnapshotRev == checkRevision:
+			print "You have already downloaded the latest Chromium snapshot.\nExiting..."
+			sys.exit()
+				
+		elif SnapshotRev > checkRevision:
+			print "The last revision you have downloaded is outdated.\nUpdating..."
+			
+	else:
+		print "You have not yet downloaded any Chromium snapshot, or they are stored in another directory. Downloading..."
+			
+	openRevisionFile.close()
+		
+def GetSnapshot(osStringAppend, SnapshotRev, dlfile, userDLDir):
+	SnapshotURL = 'https://commondatastorage.googleapis.com/chromium-browser-snapshots/' + osStringAppend + '/' + SnapshotRev + '/' + dlfile
 	print "Download URL: %s" % SnapshotURL
+	
 	try:
 		reachSnapshotFile = urllib2.urlopen(SnapshotURL, timeout=30)
+		os.chdir(userDLDir)
 		downloadSnapshot = urllib.urlretrieve(SnapshotURL, dlfile, reporthook=dlProgress)
 		reachSnapshotFile.close()
 	except urllib2.URLError:
@@ -121,19 +198,20 @@ def getSnapshot():
 		
 def dlProgress(count, blockSize, totalSize):
 	percent = int(count*blockSize*100/totalSize)
-	sys.stdout.write("\r" + "Downloading Chromium r%s... " % SnapshotVer + " %d%%" % percent)
+	sys.stdout.write("\r" + "Downloading Chromium r%s... " % SnapshotRev + " %d%%" % percent)
 	sys.stdout.flush()
 		
-def logNewInstall():
-	
-	updateRevisionFile = open(revisionFile, 'w+')
-	updateRevisionFile.write(SnapshotVer)
+def LogNewInstall(RevisionFile, SnapshotRev):
+	updateRevisionFile = open(RevisionFile, 'w+')
+	updateRevisionFile.write(SnapshotRev)
 	updateRevisionFile.close()
 
 if __name__ == '__main__':
-	createStringFromOS()
-	findSnapshotVer()
-	checkPriorInstall()
-	getSnapshot()
-	logNewInstall()
+	BuildURLFromOS()
+	osStringAppend, dlfile = BuildURLFromOS()
+	SnapshotRev = FindSnapshotRevision(osStringAppend)
+	RevisionFile, userDLDir = CheckDirsFiles()
+	CheckPriorRevision(RevisionFile, SnapshotRev)
+	GetSnapshot(osStringAppend, SnapshotRev, dlfile, userDLDir)
+	LogNewInstall(RevisionFile, SnapshotRev)
 	print "\nDone. Exiting...\r"
